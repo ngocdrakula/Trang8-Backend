d1ec725d91964ad5c05d1c6fcc265572e00aa417
ngocdrakula@gmail.com <ngocdrakula@gmail.com>
2020-04-25 19:13:36 +0700

    23

--- app.js
+++ app.js
@@ -4,24 +4,6 @@ const app = express();
 app.use(express.static("File"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json({}));
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/trang8',
-    {
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    },
-    (err) => {
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log("DB conected!")
-        }
-    }
-);
-
 server = app.listen(process.env.PORT || 3000, (err) => {
     if(err){
         console.log(err);
