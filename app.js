c058e31d949483ee41198d3f8fb302235c3fa12c
ngocdrakula@gmail.com <ngocdrakula@gmail.com>
2020-04-25 18:45:09 +0700

    23

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser');
 app.use(express.static("File"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json({}));
+server = app.listen(process.env.PORT || 3000, (err) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Server start!");
+    }
+});
 app.get("/", (req, res) => {
     res.sendFile("HTML/index.html", { root: __dirname });
 });
