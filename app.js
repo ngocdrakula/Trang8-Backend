e4eebd93522db5331533aa3076ecd1a1d0f65297
ngocdrakula@gmail.com <ngocdrakula@gmail.com>
2020-04-25 18:26:39 +0700

    23

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const bodyParser = require('body-parser');
 app.use(express.static("File"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json({}));
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.sendFile("HTML/index.html", { root: __dirname });
 });
