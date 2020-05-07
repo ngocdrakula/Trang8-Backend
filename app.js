6d4aeb1d0ea6b9fd1e8ce95f6b05cf81e35fed70
ngocdrakula@gmail.com <ngocdrakula@gmail.com>
2020-05-07 22:35:47 +0700

    Fix cors

--- app.js
+++ app.js
@@ -32,7 +32,13 @@ const app = express();
 
 app.use(cors({
     origin:['http://localhost:3000'],
-    credentials: true}))
+    credentials: true
+}));
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
 app.use(session);
 if(0){
     app.use('', (req, res, next) => {
